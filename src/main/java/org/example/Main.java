545ed5b3699abc543c10d1a1ff6eb9000fc3b9b5
bhuvan <bhuvanapraap@gmail.com>
2023-07-05 16:10:10 +0200

    modified

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,18 +11,18 @@ public class Main {
         System.out.println("Welcome to the calculator app");
         System.out.println("*****************************");
         System.out.println("Choose the operation");
-        while (true) {
 
-            menu();
-            option = input.nextInt();
-            if (option <= 4) {
+        while (true) {
+        menu();
+        option = input.nextInt();
+            if (option <= 4)
+            {
+
             System.out.println("Enter the first number");
             no1 = input.nextDouble();
             System.out.println("Enter the second number");
             no2 = input.nextDouble();
-
-
-                switch (option) {
+            switch (option) {
                     case 1:
                         result = no1 + no2;
                         System.out.println(no1 + "+" + no2 + "=" + result);
@@ -41,6 +41,7 @@ public class Main {
                         break;
                     default:
                         System.out.println("Entered wrong option");
+                        break;
 
                 }
 
@@ -51,8 +52,6 @@ public class Main {
                 System.out.println("thanks for using calculator");
                 break;
             }
-
-
 
         }
 
