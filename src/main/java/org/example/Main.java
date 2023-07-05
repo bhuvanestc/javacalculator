a601ed385a2c03632a40914ae0fdad5b2e434236
bhuvan <bhuvanapraap@gmail.com>
2023-07-05 15:55:05 +0200

    updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,43 +11,60 @@ public class Main {
         System.out.println("Welcome to the calculator app");
         System.out.println("*****************************");
         System.out.println("Choose the operation");
-        menu();
-        option = input.nextInt();
-        System.out.println("Enter the first number");
-        no1 = input.nextDouble();
-        System.out.println("Enter the second number");
-        no2 = input.nextDouble();
         while (true) {
-            switch (option) {
-                case 1:
-                    result = no1 + no2;
-                    System.out.println(no1 + "+" + no2 + "=" + result);
-                    break;
-                case 2:
-                    result = no1 - no2;
-                    System.out.println(no1 + "-" + no2 + "=" + result);
-                    break;
-                case 3:
-                    result = no1 * no2;
-                    System.out.println(no1 + "*" + no2 + "=" + result);
-                    break;
-                case 4:
-                    result = no1 - no2;
-                    System.out.println(no1 + "/" + no2 + "=" + result);
-                    break;
-                default:
-                    System.out.println("this"+option+"is not available , try other option");
-                    break;
+
+            menu();
+            option = input.nextInt();
+            if (option <= 4) {
+            System.out.println("Enter the first number");
+            no1 = input.nextDouble();
+            System.out.println("Enter the second number");
+            no2 = input.nextDouble();
+
+
+                switch (option) {
+                    case 1:
+                        result = no1 + no2;
+                        System.out.println(no1 + "+" + no2 + "=" + result);
+                        break;
+                    case 2:
+                        result = no1 - no2;
+                        System.out.println(no1 + "-" + no2 + "=" + result);
+                        break;
+                    case 3:
+                        result = no1 * no2;
+                        System.out.println(no1 + "*" + no2 + "=" + result);
+                        break;
+                    case 4:
+                        result = no1 / no2;
+                        System.out.println(no1 + "/" + no2 + "=" + result);
+                        break;
+                    default:
+                        System.out.println("Entered wrong option");
+
+                }
+
+                System.out.println("Do you want to calculate again?");
             }
+            else
+            {
+                System.out.println("thanks for using calculator");
+                break;
+            }
+
+
+
         }
 
     }
-        public static void menu ()
-        {
-            System.out.println("1.Addition");
-            System.out.println("2.Subtraction");
-            System.out.println("3.Multiplication");
-            System.out.println("4.Division");
-            System.out.println("5.Exit");
+
+            public static void menu ()
+            {
+                System.out.println("1.Addition");
+                System.out.println("2.Subtraction");
+                System.out.println("3.Multiplication");
+                System.out.println("4.Division");
+            }
+
+
         }
-    }
