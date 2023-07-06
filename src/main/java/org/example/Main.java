1a8ab6ecae2586f072219f95d7960117c08dc43b
bhuvan <bhuvanapraap@gmail.com>
2023-07-06 15:25:01 +0200

    modified

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,19 +10,18 @@ public class Main {
         System.out.println("*****************************");
         System.out.println("Welcome to the calculator app");
         System.out.println("*****************************");
-        System.out.println("Choose the operation");
+        boolean exit = false;
+        while (!exit) {
+            menu();
+            System.out.println("Please choose the operation between 1 to 5");
+            option = input.nextInt();
 
-        while (true) {
-        menu();
-        option = input.nextInt();
-            if (option <= 4)
-            {
-
-            System.out.println("Enter the first number");
-            no1 = input.nextDouble();
-            System.out.println("Enter the second number");
-            no2 = input.nextDouble();
-            switch (option) {
+            if (option >= 1) {
+                System.out.println("Enter the first number");
+                no1 = input.nextDouble();
+                System.out.println("Enter the second number");
+                no2 = input.nextDouble();
+                switch (option) {
                     case 1:
                         result = no1 + no2;
                         System.out.println(no1 + "+" + no2 + "=" + result);
@@ -39,31 +38,29 @@ public class Main {
                         result = no1 / no2;
                         System.out.println(no1 + "/" + no2 + "=" + result);
                         break;
+                    case 5:
+                        exit = true;
+                        System.out.println("Exit ");
+                        break;
                     default:
                         System.out.println("Entered wrong option");
-                        break;
 
                 }
-
-                System.out.println("Do you want to calculate again?");
+            } else {
+                System.out.println("Thanks for using calculator");
             }
-            else
-            {
-                System.out.println("thanks for using calculator");
-                break;
-            }
-
         }
 
+        System.out.println("Thanks for using calculator");
     }
 
-            public static void menu ()
-            {
-                System.out.println("1.Addition");
-                System.out.println("2.Subtraction");
-                System.out.println("3.Multiplication");
-                System.out.println("4.Division");
-            }
+    public static void menu() {
+        System.out.println("1.Addition");
+        System.out.println("2.Subtraction");
+        System.out.println("3.Multiplication");
+        System.out.println("4.Division");
+        System.out.println("5.Exit");
+    }
+}
 
 
-        }
